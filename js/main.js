0261b68d06e9dcec53c23adc45ce34b1f826ad7e
Natalia Pilipchuk <ntpylypchuk@gmail.com>
2017-07-24 12:08:08 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    navText: ["",""],
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            dots: true,
+            nav:true
+        },
+        600:{
+            items:1,
+            dots: true,
+            nav:true
+        },
+        1000:{
+            items:1,
+            dots: true,
+            nav:true
+        }
+    }
+});
